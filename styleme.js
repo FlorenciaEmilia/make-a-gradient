00e0d56141a5a4d870f12b9ea5e95574adf20ce8
Florencia <flxrenciaemilia@gmail.com>
2020-07-25 11:20:27 -0500

    Addedd gradient information when page loads

--- styleme.js
+++ styleme.js
@@ -1,16 +1,24 @@
-const inputs=document.querySelectorAll('.controlledInput input');
-const backg=document.querySelector('#box');
+const inputs = document.querySelectorAll('.controlledInput input');
+const backg = document.querySelector('#box');
+const range = document.querySelector('#degree');
+const gradientInfo = document.querySelector('#gradientOutput');
+// const initialStsate = document.querySelector('body');
+window.addEventListener('load', testingThings);
 
-function handleUpdate(){
-    const deg=this.dataset.sizing||'';
-    
-    document.documentElement.style.setProperty(`--${this.name}`, this.value+deg)
+function testingThings() {
+	const style = getComputedStyle(backg);
+	const backgrounnd = style.background.split(' ').slice(4, 13).join(' ');
 
-    const style=getComputedStyle(backg)
-    const backgrounnd=style.background.split(" ").slice(4,13).join(" ")
-    
-    document.getElementById("gradientOutput").innerHTML = `${backgrounnd}`
+	gradientInfo.innerHTML = `${backgrounnd}`;
 }
 
-inputs.forEach(input=>input.addEventListener('input', handleUpdate))
-inputs.forEach(input=>input.addEventListener('input', handleUpdate))
+function handleUpdate() {
+	const deg = this.dataset.sizing || '';
+
+	document.documentElement.style.setProperty(`--${this.name}`, this.value + deg);
+
+	testingThings();
+}
+
+inputs.forEach((input) => input.addEventListener('input', handleUpdate));
+inputs.forEach((input) => input.addEventListener('input', handleUpdate));
